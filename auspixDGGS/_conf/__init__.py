a51cc32502e20daf4b608ef582632e6e737dd76f
Joseph Bell <joseph.bell@ga.gov.au>
2021-03-03 14:21:48 +1100

    removed experimental connect to DB
    removed DB connection

--- auspixDGGS/_conf/__init__.py
+++ auspixDGGS/_conf/__init__.py
@@ -18,28 +18,26 @@ DEBUG = True
 
 DGGS_PID_PREFIX = 'ec2-13-238-161-97-ap-southeast-2.compute.amazonaws.com/AusPIX-DGGS-dataset/'
 
-# get db conn settings from yaml file
-directory = os.path.dirname(os.path.realpath(__file__))
-file = os.path.join(directory, "secrets.yml")
-DB_CON_DICT = yaml.safe_load(open(file))
-
-
-print('dbcon', list(DB_CON_DICT))
-
-
-if DB_CON_DICT is None:
-    print('You must set up a secrets.yml file containing the DB login credentials')
-    exit()
-
-
-def db_select(q):
-    print('q', q)
-    try:
-        conn = psycopg2.connect(**DB_CON_DICT['db_con'])
-        print('db_select con', conn)
-
-        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        cur.execute(q)
-        return cur.fetchall()
-    except Exception as e:
-        print(e)
+# # get db conn settings from yaml file
+# directory = os.path.dirname(os.path.realpath(__file__))
+# file = os.path.join(directory, "old_secrets.yml")
+# DB_CON_DICT = yaml.safe_load(open(file))
+# print('dbcon', DB_CON_DICT)
+#
+#
+# if DB_CON_DICT is None:
+#     print('You must set up a old_secrets.yml file containing the DB login credentials')
+#     exit()
+#
+#
+# def db_select(q):
+#     print('q', q)
+#     try:
+#         conn = psycopg2.connect(**DB_CON_DICT['db_con'])
+#         print('con', conn)
+#
+#         cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+#         cur.execute(q)
+#         return cur.fetchall()
+#     except Exception as e:
+#         print(e)
